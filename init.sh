522bf1efdf6e525be4d453fe6cc035fd8d76b679
EricTtTtT <erictien02@gmail.com>
2021-12-10 01:01:54 +0800

    🐛 quick fix

--- init.sh
+++ init.sh
@@ -5,23 +5,24 @@ PERSONACHAT_SELF_ORIGIN=data/personachat_self_original.json
 
 GPT_PERSONACHAT_URL=https://s3.amazonaws.com/models.huggingface.co/transfer-learning-chatbot/gpt_personachat_cache.tar.gz
 GPT_PERSONACHAT_CACHE=tmp/gpt_personachat_cache.tar.gz
-GPT_PERSONACHAT_MODEL=model/gpt2_personachat/
+GPT_PERSONACHAT_MODEL=model/gpt2_personachat
 
 ENGAGING_CLASSIFIER_CACHE=tmp/engaging_classifier.tar.gz
-ENGAGING_CLASSIFIER_MODEL=model/engaging_classifier/
+ENGAGING_CLASSIFIER_MODEL=model/engaging_classifier
 
 mkdir data
 mkdir model
 mkdir sample
 mkdir tmp
 mkdir $GPT_PERSONACHAT_MODEL
+mkdir $ENGAGING_CLASSIFIER_MODEL
 
-if [! -f $PERSONACHAT_SELF_ORIGIN ]; then
+if [ ! -f $PERSONACHAT_SELF_ORIGIN ]; then
     echo "Downloading personachat dataset"
     wget $PERSONACHAT_SELF_URL -O $PERSONACHAT_SELF_ORIGIN
 fi
 
-if [! -f $GPT_PERSONACHAT_CACHE ]; then
+if [ ! -f $GPT_PERSONACHAT_CACHE ]; then
     echo "Downloading GPT-2 model"
     wget $GPT_PERSONACHAT_URL -O $GPT_PERSONACHAT_CACHE
     echo "Extracting GPT-2 model"
